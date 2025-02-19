6909e0e69ffc95dd71e6a124f735a9dafd2448e1
Ashish Yadav <159691919+ASHISHRYADAV15@users.noreply.github.com>
2025-02-20 03:23:15 +0530

    Add files via upload

---
+++ datatypes2.java
@@ -0,0 +1,24 @@
+public class datatypes2 {
+    public static void main(String[] args) {
+        // data Types
+        // char first = 'a';
+        // Byte rollno = 50;
+        // short age = 23;
+        // int seatNum = 10128072;
+        // float cgpa = 8.1f;
+        // System.out.println("hello, " + first + "is your rollno is " + rollno + "and your age is " + age + ", check your seatNumber " + seatNum + ", your cgpa " + cgpa + " is good");
+        int myNumber = 18;
+        int yourNumber = 865;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+        System.out.println(yourNumber);
+
+        boolean isVegetarian = false;
+        System.out.println(isVegetarian);
+
+        float myfloat = 5.0f;
+        System.out.println(myfloat);
+        int a$b = 5;
+        System.out.println(a$b);
+    }
+}
